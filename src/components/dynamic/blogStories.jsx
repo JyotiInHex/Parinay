bac206d9c2257db1912037577ea34ce676f60854
JyotiInHex <paragjyoti43d@gmail.com>
2024-07-04 20:45:25 +0530

    New Update

--- src/components/dynamic/blogStories.jsx
+++ src/components/dynamic/blogStories.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import stories from '../../assets/parinay-desktop-banner 5.webp';
+import stories from '../../assets/8.jpg';
 
 const blogStories = () => {
   return (
     <div className='mt-[5em]'>
       <div className='mb-5 text-xl text-zinc-400 font-opensans font-medium underline transition-all'>(03) Our Blog's and Stories </div>
-      <div className='relative p-12 w-full max-w-full h-auto bg-slate-100 rounded-t-3xl mb-5'>
+      <div className='relative p-12 w-full max-w-full h-auto bg-slate-100 rounded-3xl mb-5'>
         <h2 className='text-5xl text-zinc-900 font-semibold font-merriweather leading-normal'>Love Chronicles, Stories, Tips, and Insights form Our Dating Site Blog</h2>
         <div className='flex flex-row gap-4 mt-9'>
           <div className='blog max-w-[25%] h-fit flex flex-col justify-between gap-7 bg-slate-200 p-7 rounded-3xl'>
@@ -18,14 +18,18 @@ const blogStories = () => {
             <p className='text-lg text-slate-200 font-lato font-normal'>Experts insights into navigating the digital dating landscape.</p>
           </div>
 
-          <div className='stores relative overflow-hidden w-full h-fit flex flex-col gap-7 bg-slate-200 rounded-3xl'>
+          <div className='stores group relative overflow-hidden w-full h-fit flex flex-col gap -7 bg-slate-200 rounded-3xl'>
             <figure className='top-0 left-0 w-full h-[50vh] max-h-[50vh]'>
               <img className='w-full h-full object-cover' src={stories} alt="storie" />
             </figure>          
             <figcaption className='absolute p-5 bottom-0'>
-              <p className='text-lg text-justify text-white font-lato font-medium text-shadow-4xl mb-3'>"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam, facere saepe. Incidunt autem pariatur, id explicabo officia sapiente dolor molestias aperiam repellat. Fugiat laudantium nemo quisquam illum vero soluta cumque."</p>
-              <h5 className='text-base text-white font-lato font-medium text-shadow-2xl'>-Rohit and Dipika</h5>
+              <p className='text-lg text-justify text-white font-lato font-medium text-shadow-4xl mb-3'>"We met through this website and it was love at first sight. Thank you for bringing us together."</p>
+              <h5 className='text-base text-white font-lato font-medium text-shadow-2xl'>-Amit & Priya</h5>
             </figcaption>
+            <dir className='absolute right-5 bottom-0 flex flex-row gap-3 opacity-0 transition-all group-hover:opacity-100'>
+              <button className='border-2 border-slate-200 bg-slate-200 rounded-3xl text-lg font-bold text-zinc-900 w-12'><i className='ri-arrow-left-line'></i></button>
+              <button className='border-2 border-slate-200 bg-slate-200 rounded-3xl text-lg font-bold text-zinc-900 w-12'><i className='ri-arrow-right-line'></i></button>
+            </dir>
           </div>
         </div>
       </div>
