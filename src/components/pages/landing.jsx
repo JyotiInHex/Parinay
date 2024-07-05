3495ff3c590df75c23d04a37d44094b572906e5a
JyotiInHex <paragjyoti43d@gmail.com>
2024-07-05 13:18:38 +0530

    New update

--- src/components/pages/landing.jsx
+++ src/components/pages/landing.jsx
@@ -4,6 +4,7 @@ import Profiles from "../dynamic/profiles";
 import About from "../dynamic/about";
 import Features from "../static/features";
 import BlogStories from "../dynamic/blogStories";
+import GetStarted from "../dynamic/getStarted";
 
 const Landing = () => {
   return (
@@ -80,14 +81,7 @@ const Landing = () => {
         <About/>
         <BlogStories/>
       </div>
-      <div className="relative mb-9">
-        <div className="w-full h-auto bg-[#FFF5E1] p-12 flex flex-col gap-6 justify-between">
-          <div className='mb-5 text-xl text-zinc-400 font-opensans font-medium underline transition-all'>(04) Get Started </div>
-          <h2 className="w-[75%] text-5xl text-left text-zinc-900 font-merriweather font-semibold leading-relaxed">Embark on your journey to find true love <i className="ri-chat-heart-fill text-[#E91E63]"></i> with Parinay.com. Creating your profile is quick and easy, allowing you to showcase your personality and preferences.</h2>
-          <p className="w-[55%] self-end text-xl text-justify text-zinc-900 text-shadow-md font-lato font-medium">Discover a community of like-minded Assamese singles. Our user-friendly interface ensures a seamless experience as you navigate through profiles and features. Join Parinay.com today and take the first step towards finding <i className="ri-search-2-line text-[#FFC107] font-bold"></i> your perfect match.</p>
-          <button className="relative z-10 overflow-hidden group border-2 border-[#FF6F61] w-fit h-fit self-end px-8 py-3 rounded-full text-xl text-[#FF6F61] font-lato font-semibold after:transition-all after:absolute after:-bottom-14 after:z-[-1] after:left-0 after:w-full after:h-full after:rounded-[50%] after:bg-white hover:after:-bottom-0 hover:after:rounded-none"><Link>Begin Your Journey <i className="ri-arrow-right-line transition-all group-hover:ml-4"></i></Link></button>
-        </div>
-      </div>
+      <GetStarted/>
     </>
   )
 }
