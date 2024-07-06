a1f8ae2335c3631b0437f798c2a5ee367ee6c224
JyotiInHex <paragjyoti43d@gmail.com>
2024-07-06 22:44:10 +0530

    today last

--- src/pages/aboutUs.jsx
+++ src/pages/aboutUs.jsx
@@ -4,9 +4,10 @@ const aboutUs = () => {
   return (
     <>
       <div className='w-full h-auto p-5'>
-        <figure className='bg-black p-6 mr-5 ml-5 rounded-3xl'>
-          <h1 className='pointer-events-none text-9xl text-gray-300 text-shadow-sm opacity-50 font-bold font-opensans relative'>About Us</h1> 
+        <figure className='p-6 mr-5 ml-5 rounded-3xl'>
+          <h1 className='pointer-events-none text-9xl text-gray-500 text-shadow-sm font-bold font-opensans relative'>About Us</h1> 
         </figure>
+        <h6 className='px-5 text-lg text-justify text-zinc-900 font-lato font-semibold'>Welcome to Parinay.com, Assam's premier dating app designed to connect hearts and build meaningful relationships. Rooted in the rich culture and traditions of Assam, Parinay.com is more than just a dating platform; it's a space where singles can find love, companionship, and lasting bonds. At Parinay.com, we understand the unique nuances of Assamese heritage and values. Our platform is tailored to bring together like-minded individuals who share a common cultural background, making it easier to find someone who resonates with your lifestyle and beliefs.</h6>
       </div>
     </>
   )
