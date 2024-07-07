e65964ce2d4049b8d614de26c85c3e210ede7c7c
JyotiInHex <paragjyoti43d@gmail.com>
2024-07-07 21:02:35 +0530

    New Update

--- src/pages/aboutUs.jsx
+++ src/pages/aboutUs.jsx
@@ -6,7 +6,7 @@ const aboutUs = () => {
     <>
       <div className='w-full h-auto p-5'>
         <figure className='p-6 mr-5 ml-5 mb-5 rounded-3xl bg-gradient-to-tr'>
-          <h1 className='text-9xl text-gray-700 text-shadow-sm font-bold font-opensans relative'><sub className='text-base font-normal cursor-pointer underline'><Link to={"/"}>(Home)</Link></sub>About Us</h1> 
+          <h1 className='text-9xl text-gray-700 text-shadow-sm font-bold font-opensans relative'><sup className='text-base font-semibold cursor-pointer underline'><Link to="/">(Home)</Link></sup>About Us</h1> 
         </figure>
         <div className='bg-white w-full h-full p-10 rounded-2xl text-lg text-zinc-700 font-lato font-medium'>
           <h6 className='px-5 text-justify'>Welcome to <strong>Parinay</strong>, where we unite hearts within the rich cultural tapestry of Assam. <strong>Parinay</strong> is Assam's premier dating app designed to connect hearts and build meaningful relationships. Rooted in the rich culture and traditions of Assam, <strong>Parinay</strong> goes beyond being just a dating platform; it's a space where singles can find love, companionship, and lasting bonds. At <strong>Parinay</strong>, we understand the unique nuances of Assamese heritage and values. Our platform is tailored to bring together like-minded individuals who share a common cultural background, making it easier to find someone who resonates with your lifestyle and beliefs.</h6>
--- src/pages/services.jsx
+++ src/pages/services.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const services = () => {
   return (
-    <div>services</div>
+    <>
+      <div className='w-full h-auto p-5'>
+        <figure className='p-6 mr-5 ml-5 mb-5 rounded-3xl bg-gradient-to-tr'>
+          <h1 className='text-9xl text-gray-700 text-shadow-sm font-bold font-opensans relative'><sup className='text-base font-semibold cursor-pointer underline'><Link to="/">(Home)</Link></sup>Services</h1> 
+        </figure>
+        <h6>At Parinay.com, we flatter ourselves on offering a comprehensive set of features that enhance your online courting experience. Our platform is designed to make chancing your perfect Assamese match both pleasurable and effective. Then’s what you can anticipate from our point-rich app -</h6>
+      </div>
+    </>
   )
 }
 
