24e79d8611438b89f4bba0be8de82fa08a43cda0
JyotiInHex <paragjyoti43d@gmail.com>
2024-07-08 22:31:18 +0530

    new update

--- src/pages/aboutUs.jsx
+++ src/pages/aboutUs.jsx
@@ -15,36 +15,40 @@ const aboutUs = () => {
           <div className='mt-8 mb-3 px-5'>
             <h5 className='text-2xl'>Why Choose Parinay.com?</h5>
             <div className='w-full h-auto py-5 drop-shadow-xl flex gap-5'>
-              <div className='overflow-hidden group w-[25%] h-auto rounded-2xl bg-[#B2DFDB] p-5'>
-                <i className='ri-earth-fill block text-4xl text-zinc-800 h-[80%] transition-all group-hover:h-[23%]'></i>
-                <h5 className='relative flex justify-between items-center text-[1em] text-zinc-700 leading-6 font-opensans font-semibold border-b-2 border-transparent transition-all group-hover:border-zinc-700'><sup className='absolute -top-5 text-lg text-zinc-700 font-opensans font-bold'>01.</sup>Cultural Connection<span className='ri-arrow-right-up-line text-3xl font-normal transition-all group-hover:rotate-90'></span></h5>
+              <div className='relative z-10 overflow-hidden group w-[25%] h-auto rounded-2xl bg-[#B2DFDB] p-5'>
+                <i className='ri-earth-fill block text-4xl text-slate-600 h-[80%] transition-all group-hover:h-[23%]'></i>
+                <span className='absolute top-[50%] z-[-1] text-8xl text-slate-50 opacity-20 font-opensans font-bold'>01.</span>
+                <h5 className='flex justify-between items-center text-[1em] text-zinc-700 leading-6 font-opensans font-semibold border-b-2 border-transparent transition-all group-hover:border-zinc-700'>Cultural Connection<span className='ri-arrow-right-up-line text-3xl font-normal transition-all group-hover:rotate-90'></span></h5>
                 <p className='mb-[1.3em] text-base text-zinc-700 font-lato font-semibold w-full h-fit opacity-0 transition-all  group-hover:opacity-100'>We celebrate Assamese culture and create opportunities for you to meet others who appreciate and cherish it as much as you do.</p>
               </div>
 
-              <div className='overflow-hidden group w-[25%] h-auto rounded-2xl bg-[#FFF9C4] p-5'>
-                <i className='ri-lightbulb-fill block text-4xl text-zinc-800 h-[80%] transition-all group-hover:h-[23%]'></i>
-                <h5 className='relative flex justify-between items-center text-[1em] text-zinc-700 leading-6 font-opensans font-semibold border-b-2 border-transparent transition-all group-hover:border-zinc-700'><sup className='absolute -top-5 text-lg text-zinc-700 font-opensans font-bold'>02.</sup>User-Friendly Interface <span className='ri-arrow-right-up-line text-3xl font-normal transition-all group-hover:rotate-90'></span></h5>
+              <div className='relative z-10 overflow-hidden group w-[25%] h-auto rounded-2xl bg-[#bdbefc] p-5'>
+                <i className='ri-lightbulb-fill block text-4xl text-slate-600 h-[80%] transition-all group-hover:h-[23%]'></i>
+                <span className='absolute top-[50%] z-[-1] text-8xl text-slate-50 opacity-20 font-opensans font-bold'>02.</span>
+                <h5 className='relative flex justify-between items-center text-[1em] text-zinc-700 leading-6 font-opensans font-semibold border-b-2 border-transparent transition-all group-hover:border-zinc-700'>User-Friendly Interface <span className='ri-arrow-right-up-line text-3xl font-normal transition-all group-hover:rotate-90'></span></h5>
                 <p className='mb-[1.3em] text-base text-zinc-700 font-lato font-semibold w-full h-fit opacity-0 transition-all  group-hover:opacity-100'>Our app is designed to be intuitive and easy to navigate, ensuring a seamless experience as you search for your perfect match.</p>
               </div>
 
-              <div className='overflow-hidden group w-[25%] h-auto rounded-2xl bg-[#BBDEFB] p-5'>
-                <i className='ri-user-follow-fill block text-4xl text-zinc-800 h-[80%] transition-all group-hover:h-[23%]'></i>
-                <h5 className='relative flex justify-between items-center text-[1em] text-zinc-700 leading-6 font-opensans font-semibold border-b-2 border-transparent transition-all group-hover:border-zinc-700'><sup className='absolute -top-5 text-lg text-zinc-700 font-opensans font-bold'>03.</sup>Verified Profiles <span className='ri-arrow-right-up-line text-3xl font-normal transition-all group-hover:rotate-90'></span></h5>                
+              <div className='relative z-10 overflow-hidden group w-[25%] h-auto rounded-2xl bg-[#BBDEFB] p-5'>
+                <i className='ri-user-follow-fill block text-4xl text-slate-600 h-[80%] transition-all group-hover:h-[23%]'></i>
+                <span className='absolute top-[50%] z-[-1] text-8xl text-slate-50 opacity-20 font-opensans font-bold'>03.</span>
+                <h5 className='relative flex justify-between items-center text-[1em] text-zinc-700 leading-6 font-opensans font-semibold border-b-2 border-transparent transition-all group-hover:border-zinc-700'>Verified Profiles <span className='ri-arrow-right-up-line text-3xl font-normal transition-all group-hover:rotate-90'></span></h5>                
                 <p className='mb-[1.3em] text-base text-zinc-700 font-lato font-semibold w-full h-fit opacity-0 transition-all  group-hover:opacity-100'>We prioritize your safety and peace of mind. All profiles are thoroughly verified to ensure genuine connections.</p>
               </div>
 
-              <div className='overflow-hidden group w-[25%] h-auto rounded-2xl bg-[#C8E6C9] p-5'>
-                <i className='ri-function-fill block text-4xl text-zinc-800 h-[80%] transition-all group-hover:h-[23%]'></i>
-                <h5 className='relative flex justify-between items-center text-[1em] text-zinc-700 leading-6 font-opensans font-semibold border-b-2 border-transparent transition-all group-hover:border-zinc-700'><sup className='absolute -top-5 text-lg text-zinc-700 font-opensans font-bold'>04.</sup>Advanced Matching Algorithms <span className='ri-arrow-right-up-line text-3xl font-normal transition-all group-hover:rotate-90'></span></h5>                
+              <div className='relative z-10 overflow-hidden group w-[25%] h-auto rounded-2xl bg-[#C8E6C9] p-5'>
+                <i className='ri-function-fill block text-4xl text-slate-600 h-[80%] transition-all group-hover:h-[23%]'></i>
+                <span className='absolute top-[50%] z-[-1] text-8xl text-slate-50 opacity-20 font-opensans font-bold'>04.</span>
+                <h5 className='relative flex justify-between items-center text-[1em] text-zinc-700 leading-6 font-opensans font-semibold border-b-2 border-transparent transition-all group-hover:border-zinc-700'>Advanced Matching Algorithms <span className='ri-arrow-right-up-line text-3xl font-normal transition-all group-hover:rotate-90'></span></h5>                
                 <p className='mb-[1.3em] text-base text-zinc-700 font-lato font-semibold w-full h-fit opacity-0 transition-all  group-hover:opacity-100'>Our smart algorithms consider your preferences, interests, and values to suggest the best potential matches, making your journey to finding love both efficient and enjoyable.</p>
               </div>
 
-              <div className='overflow-hidden group w-[25%] h-auto rounded-2xl bg-[#FFE0B2] p-5'>
-                <i className='ri-group-fill block text-4xl text-zinc-800 h-[80%] transition-all group-hover:h-[23%]'></i>
-                <h5 className='relative flex justify-between items-center text-[1em] text-zinc-700 leading-6 font-opensans font-semibold border-b-2 border-transparent transition-all group-hover:border-zinc-700'><sup className='absolute -top-5 text-lg text-zinc-700 font-opensans font-bold'>05.</sup>Community Focused <span className='ri-arrow-right-up-line text-3xl font-normal transition-all group-hover:rotate-90'></span></h5>                
+              <div className='relative z-10 overflow-hidden group w-[25%] h-auto rounded-2xl bg-[#e0b2ff] p-5'>
+                <i className='ri-group-fill block text-4xl text-slate-600 h-[80%] transition-all group-hover:h-[23%]'></i>
+                <span className='absolute top-[50%] z-[-1] text-8xl text-slate-50 opacity-20 font-opensans font-bold'>05.</span>
+                <h5 className='relative flex justify-between items-center text-[1em] text-zinc-700 leading-6 font-opensans font-semibold border-b-2 border-transparent transition-all group-hover:border-zinc-700'>Community Focused <span className='ri-arrow-right-up-line text-3xl font-normal transition-all group-hover:rotate-90'></span></h5>                
                 <p className='mb-[1.3em] text-base text-zinc-700 font-lato font-semibold w-full h-fit opacity-0 transition-all  group-hover:opacity-100'>We foster a supportive community where users can share their experiences and celebrate their journey towards finding love.</p>
               </div>
-
             </div>
           </div> {/* dynamic */}
 
@@ -52,7 +56,7 @@ const aboutUs = () => {
 
           <div className='flex justify-between items-center mt-10 px-20'>
             <p className='text-xl font-semibold text-right'>Parinay.com – Where Assamese Hearts Connect.</p>
-            <button className='relative z-10 overflow-hidden group w-[25%] h-fit py-3 my-4 border-2 border-zinc-700 rounded-xl text-lg font-semibold flex justify-evenly items-center hover:text-slate-50 transition-all after:w-full after:h-full after:bg-zinc-900 after:absolute after:-bottom-16 after:left-0 after:z-[-1] after:rounded-[50%] after:transition-all hover:after:rounded-none hover:after:bottom-0'><Link to="/register">Join Parinay Today</Link> <i className="ri-arrow-right-up-line text-3xl font-normal transition-all group-hover:rotate-90"></i></button>
+            <button className='relative z-10 overflow-hidden group w-[20%] h-fit py-3 my-4 border-2 border-zinc-700 rounded-xl text-xl text-zinc-700 font-semibold flex justify-center items-center gap-1 hover:text-slate-50 transition-all after:w-full after:h-full after:bg-zinc-700 after:absolute after:-bottom-16 after:left-0 after:z-[-1] after:rounded-[50%] after:transition-all hover:after:rounded-none hover:after:bottom-0'><Link to="/register">Join Now</Link> <i className="ri-arrow-right-up-line text-3xl font-normal transition-all group-hover:rotate-90"></i></button>
           </div>
         </div>
       </div>
--- src/pages/features.jsx
+++ src/pages/features.jsx
@@ -22,63 +22,77 @@ const services = () => {
             anticipate from our point-rich app -
           </h6>
           <ul className="flex flex-wrap gap-x-5 gap-y-9 px-5 mt-10 mb-5">
-            <li className="relative w-[49%] h-auto bg-transparent rounded-lg border-2 p-5">
-              <i className="ri-user-search-line absolute -top-6 left-3 w-8 h-8 p-5 border-2 bg-white flex items-center justify-center rounded-lg text-2xl text-[#f44336]"></i>
-              <h5 className="text-lg text-[#f44336] font-lato font-semibold mt-3">Advanced Search Pollutants</h5>
+            <li className="relative z-10 w-[49%] h-auto bg-transparent rounded-lg border-2 p-5">
+              <i className="ri-user-search-line absolute -top-6 left-3 w-8 h-8 p-5 border-2 bg-white flex items-center justify-center rounded-lg text-2xl text-[#4169e1]"></i>
+              <span className="pointer-events-none absolute right-3 bottom-0 z-[-1] text-9xl text-slate-100 font-opensans font-semibold">01</span>
+              <h5 className="text-lg text-[#4169e1] font-lato font-semibold mt-3">Advanced Search Pollutants</h5>
               <p className="text-lg text-zinc-700 text-justify font-lato">Narrow down your hunt with customizable pollutants including age, position, interests, and more. Find someone who impeccably matches your criteria.</p>
             </li>
-            <li className="relative w-[49%] h-auto bg-transparent rounded-lg border-2 p-5">
+            <li className="relative z-10 w-[49%] h-auto bg-transparent rounded-lg border-2 p-5">
               <i className="ri-check-double-line absolute -top-6 left-3 w-8 h-8 p-5 border-2 bg-white flex items-center justify-center rounded-lg text-2xl text-[#9c27b0]"></i>
+              <span className="pointer-events-none absolute right-3 bottom-0 z-[-1] text-9xl text-slate-100 font-opensans font-semibold">02</span>
               <h5 className="text-lg text-[#9c27b0] font-lato font-semibold mt-3">Vindicated Biographies</h5>
               <p className="text-lg text-zinc-700 text-justify font-lato">Your safety is our precedence. All biographies on Parinay.com are vindicated to insure you connect with real and genuine individualities.</p>
             </li>
-            <li className="relative w-[49%] h-auto bg-transparent rounded-lg border-2 p-5">
+            <li className="relative z-10 w-[49%] h-auto bg-transparent rounded-lg border-2 p-5">
               <i className="ri-user-heart-line absolute -top-6 left-3 w-8 h-8 p-5 border-2 bg-white flex items-center justify-center rounded-lg text-2xl text-[#e91e63]"></i>
+              <span className="pointer-events-none absolute right-3 bottom-0 z-[-1] text-9xl text-slate-100 font-opensans font-semibold">03</span>
               <h5 className="text-lg text-[#e91e63] font-lato font-semibold mt-3">Smart Matching Algorithm</h5>
               <p className="text-lg text-zinc-700 text-justify font-lato">Our intelligent matching algorithm analyses your preferences, interests, and gets to suggest compatible matches, adding your chances of chancing a meaningful connection.</p>
             </li>
-            <li className="relative w-[49%] h-auto bg-transparent rounded-lg border-2 p-5">
+            <li className="relative z-10 w-[49%] h-auto bg-transparent rounded-lg border-2 p-5">
               <i className="ri-message-3-line absolute -top-6 left-3 w-8 h-8 p-5 border-2 bg-white flex items-center justify-center rounded-lg text-2xl text-[#1e88e5]"></i>
+              <span className="pointer-events-none absolute right-3 bottom-0 z-[-1] text-9xl text-slate-100 font-opensans font-semibold">04</span>
               <h5 className="text-lg text-[#1e88e5] font-lato font-semibold mt-3">In-App Messaging</h5>
               <p className="text-lg text-zinc-700 text-justify font-lato">Communicate painlessly with implicit matches through our secure and stoner-friendly messaging system. Start exchanges and get to know each other better without leaving the app.</p>
             </li>
-            <li className="relative w-[49%] h-auto bg-transparent rounded-lg border-2 p-5">
-              <i className="ri-user-settings-line absolute -top-6 left-3 w-8 h-8 p-5 border-2 bg-white flex items-center justify-center rounded-lg text-2xl text-[#673ab7]"></i>
-              <h5 className="text-lg text-[#673ab7] font-lato font-semibold mt-3">Profile Customization</h5>
+            <li className="relative z-10 w-[49%] h-auto bg-transparent rounded-lg border-2 p-5">
+              <i className="ri-user-settings-line absolute -top-6 left-3 w-8 h-8 p-5 border-2 bg-white flex items-center justify-center rounded-lg text-2xl text-[#daa520]"></i>
+              <span className="pointer-events-none absolute right-3 bottom-0 z-[-1] text-9xl text-slate-100 font-opensans font-semibold">05</span>
+              <h5 className="text-lg text-[#daa520] font-lato font-semibold mt-3">Profile Customization</h5>
               <p className="text-lg text-zinc-700 text-justify font-lato">Show your personality with detailed profile sections. Add prints, write about your interests, and punctuate what you ’re looking for in a mate to attract like- inclined individualities.</p>
             </li>
-            <li className="relative w-[49%] h-auto bg-transparent rounded-lg border-2 p-5">
+            <li className="relative z-10 w-[49%] h-auto bg-transparent rounded-lg border-2 p-5">
               <i className="ri-shield-check-line absolute -top-6 left-3 w-8 h-8 p-5 border-2 bg-white flex items-center justify-center rounded-lg text-2xl text-[#43a047]"></i>
+              <span className="pointer-events-none absolute right-3 bottom-0 z-[-1] text-9xl text-slate-100 font-opensans font-semibold">06</span>
               <h5 className="text-lg text-[#43a047] font-lato font-semibold mt-3">Sequestration and Security</h5>
               <p className="text-lg text-zinc-700 text-justify font-lato"> We value your sequestration. Parinay.com employs robust security measures to cover your data and insure a safe courting terrain.</p>
             </li>
-            <li className="relative w-[49%] h-auto bg-transparent rounded-lg border-2 p-5">
+            <li className="relative z-10 w-[49%] h-auto bg-transparent rounded-lg border-2 p-5">
               <i className="ri-user-settings-line absolute -top-6 left-3 w-8 h-8 p-5 border-2 bg-white flex items-center justify-center rounded-lg text-2xl text-[#0097a7]"></i>
+              <span className="pointer-events-none absolute right-3 bottom-0 z-[-1] text-9xl text-slate-100 font-opensans font-semibold">07</span>
               <h5 className="text-lg text-[#0097a7] font-lato font-semibold mt-3">Artistic Connection</h5>
               <p className="text-lg text-zinc-700 text-justify font-lato">Engage with a community that shares your artistic background and values. share in conversations, share gests, and celebrate Assamese traditions together.</p>
             </li>
-            <li className="relative w-[49%] h-auto bg-transparent rounded-lg border-2 p-5">
-              <i className="ri-hearts-line absolute -top-6 left-3 w-8 h-8 p-5 border-2 bg-white flex items-center justify-center rounded-lg text-2xl text-[#c2185b]"></i>
-              <h5 className="text-lg text-[#c2185b] font-lato font-semibold mt-3">Daily Match Recommendations</h5>
+            <li className="relative z-10 w-[49%] h-auto bg-transparent rounded-lg border-2 p-5">
+              <i className="ri-hearts-line absolute -top-6 left-3 w-8 h-8 p-5 border-2 bg-white flex items-center justify-center rounded-lg text-2xl text-[#6a5acd]"></i>
+              <span className="pointer-events-none absolute right-3 bottom-0 z-[-1] text-9xl text-slate-100 font-opensans font-semibold">08</span>
+              <h5 className="text-lg text-[#6a5acd] font-lato font-semibold mt-3">Daily Match Recommendations</h5>
               <p className="text-lg text-zinc-700 text-justify font-lato">Receive diurnal match recommendations grounded on your preferences and exertion, helping you discover implicit mates you might have missed.</p>
             </li>
-            <li className="relative w-[49%] h-auto bg-transparent rounded-lg border-2 p-5">
-              <i className="ri-quill-pen-line absolute -top-6 left-3 w-8 h-8 p-5 border-2 bg-white flex items-center justify-center rounded-lg text-2xl text-[#1a4961]"></i>
-              <h5 className="text-lg text-[#1a4961] font-lato font-semibold mt-3">Easy Registration</h5>
+            <li className="relative z-10 w-[49%] h-auto bg-transparent rounded-lg border-2 p-5">
+              <i className="ri-quill-pen-line absolute -top-6 left-3 w-8 h-8 p-5 border-2 bg-white flex items-center justify-center rounded-lg text-2xl text-[#ff7f50]"></i>
+              <span className="pointer-events-none absolute right-3 bottom-0 z-[-1] text-9xl text-slate-100 font-opensans font-semibold">09</span>
+              <h5 className="text-lg text-[#ff7f50] font-lato font-semibold mt-3">Easy Registration</h5>
               <p className="text-lg text-zinc-700 text-justify font-lato">Getting started with Parinay.com is quick and hassle-free. subscribe up with just a many way and begin your trip towards chancing love.</p>
             </li>
-            <li className="relative w-[49%] h-auto bg-transparent rounded-lg border-2 p-5">
-              <i className="ri-calendar-event-line absolute -top-6 left-3 w-8 h-8 p-5 border-2 bg-white flex items-center justify-center rounded-lg text-2xl text-[#4083ff]"></i>
-              <h5 className="text-lg text-[#4083ff] font-lato font-semibold mt-3">Events and Conditioning</h5>
+            <li className="relative z-10 w-[49%] h-auto bg-transparent rounded-lg border-2 p-5">
+              <i className="ri-calendar-event-line absolute -top-6 left-3 w-8 h-8 p-5 border-2 bg-white flex items-center justify-center rounded-lg text-2xl text-[#6495ed]"></i>
+              <span className="pointer-events-none absolute right-3 bottom-0 z-[-1] text-9xl text-slate-100 font-opensans font-semibold">10</span>
+              <h5 className="text-lg text-[#6495ed] font-lato font-semibold mt-3">Events and Conditioning</h5>
               <p className="text-lg text-zinc-700 text-justify font-lato">Stay updated with original events and conditioning acclimatized for the Assamese community. Join gatherings, meet new people, and make connections beyond the virtual world.</p>
             </li>
-            <li className="relative w-full h-auto bg-transparent rounded-lg border-2 p-5">
-              <i className="ri-customer-service-2-line absolute -top-6 left-3 w-8 h-8 p-5 border-2 bg-white flex items-center justify-center rounded-lg text-2xl text-[#4083ff]"></i>
-              <h5 className="text-lg text-[#4083ff] font-lato font-semibold mt-3">Client Support</h5>
+            <li className="relative z-10 w-[49%] h-auto bg-transparent rounded-lg border-2 p-5">
+              <i className="ri-customer-service-2-line absolute -top-6 left-3 w-8 h-8 p-5 border-2 bg-white flex items-center justify-center rounded-lg text-2xl text-[#cd5c5c]"></i>
+              <span className="pointer-events-none absolute right-3 bottom-0 z-[-1] text-9xl text-slate-100 font-opensans font-semibold">11</span>
+              <h5 className="text-lg text-[#cd5c5c] font-lato font-semibold mt-3">Client Support</h5>
               <p className="text-lg text-zinc-700 text-justify font-lato">Our devoted client support platoon is then to help you with any queries or enterprises, icing a smooth and pleasurable experience on our platform.</p>
             </li>
           </ul>
-          <h6 className="px-5 text-justify"><Link className="underline text-blue-700 font-semibold" to="/register">Join Parinay.com</Link> moment and explore these features designed to help you find your perfect Assamese match. Your trip to love thresholds then!</h6>
+          <div className="flex flex-col items-center gap-8 w-full h-fit">
+            <h6 className="px-5 text-left w-full text-lg text-zinc-700 font-lato font-medium">Join Parinay.com moment and explore these features designed to help you find your perfect Assamese match. Your trip to love thresholds then!</h6>
+            <button className='relative z-10 overflow-hidden group w-[20%] h-fit py-3 my-4 border-2 border-zinc-700 rounded-xl text-xl text-zinc-700 font-semibold flex justify-center items-center gap-1 hover:text-slate-50 transition-all after:w-full after:h-full after:bg-zinc-700 after:absolute after:-bottom-16 after:left-0 after:z-[-1] after:rounded-[50%] after:transition-all hover:after:rounded-none hover:after:bottom-0'><Link to="/register">Join Now</Link> <i className="ri-arrow-right-up-line text-3xl font-normal transition-all group-hover:rotate-90"></i></button>
+          </div>
         </div>
       </div>
     </>
