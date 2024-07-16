507dc2d2015e179f3822969fa5b3f7cb4f5f6eae
JyotiInHex <paragjyoti43d@gmail.com>
2024-07-16 22:52:26 +0530

    today last

--- src/auth/register.jsx
+++ src/auth/register.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import registerBg from '../assets/registration-2.webp'
 
 const Register = () => {
   const [step, setStep] = useState(1);
@@ -11,103 +12,44 @@ const Register = () => {
     step6: '',
     step7: '',
   });
+  const [animation, setAnimation] = useState(0);
+  const handleNext = () =>{
+    setAnimation('animate-slide-left')
+    setStep(step + 1);
+  }
+  const handlePrev = () =>{
+    setAnimation('animate-slide-right')
+    setStep(step - 1);
+  }
 
+  
   return (
     <>
-      <div className="relative w-full h-auto text-zinc-700 p-10">
-        <div className="p-10 w-full h-auto bg-slate-50 rounded-xl">
-            <h2 className="text-4xl font-playfair font-extrabold">Join Parinay</h2>
-            <h3 className="text-xl font-lato font-semibold mb-4">Create your account</h3>
-            <p className="text-lg font-opensans font-medium">Fill in the details below to start your journey towards finding your perfect match. Join our community and explore the possibilities.</p>
+      <div className="relative w-full h-auto p-5">
+        <div className="p-10 w-full h-auto rounded-xl flex">
+          <div className='relative z-10 w-[60%] min-h-[95vh] flex flex-col justify-end text-slate-50 p-10'>
+            <figure className='absolute top-0 left-0 z-[-1] w-full h-full overflow-hidden rounded-l-md'>
+              <img src={registerBg} alt="registerBg" className='w-full h-full object-cover pointer-events-none' />
+            </figure>
+            <span className='absolute top-0 left-0 z-[-1] w-full h-full backdrop-blur-sm rounded-md'></span>
+            <h2 className="ml-5 text-4xl font-playfair font-extrabold text-shadow-3xl">Join Parinay</h2>
+            <h3 className="ml-5 text-xl font-lato font-semibold mb-4 mt-2 text-shadow-2xl">Create your account</h3>
+            <p className="ml-5 text-lg font-opensans font-medium">Fill in the details below to start your journey towards finding your perfect match. Join our community and explore the possibilities.</p>
+          </div>
+          <form className="w-full h-auto bg-slate-50 rounded-r-md overflow-hidden" method='POST'>
+            <div className='relative w-full h-max transition-all p-7 text-zinc-700 overflow-hidden'>
+                <div className={animation}>
+                  <h2 className='flex justify-between items-end text-3xl font-playfair font-extrabold border-b-2 border-gray-600 pb-2'>Account Details
+                    <i className='ri-arrow-right-s-line text-end font-medium '></i>
+                  </h2>
+                  {/* fieldset */}
+                  <div className=''>
+                    <label htmlFor=""></label>
+                  </div>
+                </div>      
+            </div>
+          </form>
         </div>
-        <form className="w-full h-auto p-7 mt-4 bg-slate-50 rounded-lg drop-shadow-lg overflow-hidden" method='POST'>
-              <div className='relative w-full h-max min-h-[55vh] transition-all'>
-                {step === 1 && (
-                  <div className='grid grid-cols-2 gap-5 w-full min-w-full'>
-                    <div className='col-start-1 col-end-3 flex items-center justify-between'>
-                      <h3 className='space-y-4 text-xl text-zinc-700 font-semibold font-merriweather'>Account Details</h3>
-                      <span className='ri-arrow-right-s-line cursor-pointer text-2xl text-zinc-700 font-medium' onClick={() => setStep(step + 1)}></span>
-                    </div>
-
-                    <div className="drop-shadow-md">
-                        <label className="block text-lg font-lato font-semibold mb-1" for="name">Full Name</label>
-                        <input className="outline-none border-2 border-zinc-200 rounded-xl w-full py-3 px-4 text-lg font-lato font-semibold text-zinc-800 leading-tight transition-all focus:border-blue-500" id="name" type="text" name="name" required/>
-                    </div>
-                    <div className="drop-shadow-md">
-                        <label className="block text-lg font-lato font-semibold mb-1" for="email">Email</label>
-                        <input className="outline-none border-2 border-zinc-200 rounded-xl w-full py-3 px-4 text-lg font-lato font-semibold text-zinc-800 leading-tight transition-all focus:border-blue-500" id="email" type="email" name="email" required/>
-                    </div>
-                    <div className="drop-shadow-md">
-                      <label className="block text-lg font-lato font-semibold mb-1" for="password">Password</label>
-                      <input className="outline-none border-2 border-zinc-200 rounded-xl w-full py-3 px-4 text-lg font-lato font-semibold text-zinc-800 leading-tight transition-all focus:border-blue-500" id="password" type="password" name="password" required/>
-                    </div>
-                    <div className="drop-shadow-md">
-                      <label className="block text-lg font-lato font-semibold mb-1" for="conPassword">Confirm Password</label>
-                      <input className="outline-none border-2 border-zinc-200 rounded-xl w-full py-3 px-4 text-lg font-lato font-semibold text-zinc-800 leading-tight transition-all focus:border-blue-500" id="conPassword" type="password" name="conPassword" required/>
-                    </div>
-                  </div>
-                )}
-                {step === 2 && (
-                  <div className='grid grid-cols-2 gap-5 w-full min-w-full '>
-                    <div className='col-start-1 col-end-3 flex items-center justify-between'>
-                      <span className='ri-arrow-left-s-line cursor-pointer text-2xl text-zinc-700 font-medium' onClick={() => setStep(step - 1)}></span>
-                      <h3 className='space-y-4 text-xl text-zinc-700 font-semibold font-merriweather'>Profile Information</h3>
-                      <span className='ri-arrow-right-s-line cursor-pointer text-2xl text-zinc-700 font-medium' onClick={() => setStep(step + 1)}></span>
-                    </div>
-                    
-                    <div className="drop-shadow-md">
-                      <label className="block text-lg font-lato font-semibold mb-1" for="gender">Gender</label>
-                      <select name="gender" id="gender" className="outline-none border-2 border-zinc-200 rounded-xl w-full py-3 px-4 text-lg font-lato font-semibold text-zinc-800 leading-tight transition-all focus:border-blue-500">
-                        <option value="null" selected disabled hidden>Select</option>
-                        <option value="women">Women</option>
-                        <option value="men">Men</option>
-                        <option value="other">Other</option>
-                      </select>
-                    </div>
-                    <div className="drop-shadow-md">
-                      <label className="block text-lg font-lato font-semibold mb-1" for="maritalStatus">Marital Status</label>
-                      <select name="maritalStatus" id="maritalStatus" className="outline-none border-2 border-zinc-200 rounded-xl w-full py-3 px-4 text-lg font-lato font-semibold text-zinc-800 leading-tight transition-all focus:border-blue-500">
-                        <option value="null" selected disabled hidden>Select</option>
-                        <option value="never married">Never Married</option>
-                        <option value="divorced">Divorced</option>
-                        <option value="widowed">Widowed</option>
-                        <option value="separated">Separated</option>
-                      </select>
-                    </div>
-                    <div className="drop-shadow-md">
-                      <label className="block text-lg font-lato font-semibold mb-1" for="dob">Date of Birth</label>
-                      <input className="outline-none border-2 border-zinc-200 rounded-xl w-full py-3 px-4 text-lg font-lato font-semibold text-zinc-800 leading-tight transition-all focus:border-blue-500" id="dob" type="date" name="dob" required/>
-                    </div>
-                    <div className="drop-shadow-md">
-                      <label className="block text-lg font-lato font-semibold mb-1" for="phone">Phone Number</label>
-                      <input className="outline-none border-2 border-zinc-200 rounded-xl w-full py-3 px-4 text-lg font-lato font-semibold text-zinc-800 leading-tight transition-all focus:border-blue-500" id="phone" type="tel" name="phone" required/>
-                    </div>
-                  </div>
-                )}
-                {step === 3 && (
-                <div className='grid grid-cols-2 gap-5 w-full min-w-full'>
-                  <div className='col-start-1 col-end-3 flex items-center justify-between'>
-                    <span className='ri-arrow-left-s-line cursor-pointer text-2xl text-zinc-700 font-medium' onClick={() => setStep(step - 1)}></span>
-                    <h3 className='space-y-4 text-xl text-zinc-700 font-semibold font-merriweather'>Location</h3>
-                  </div>
-
-                  <div className="drop-shadow-md">
-                    <label className="block text-lg font-lato font-semibold mb-1" for="country">Country</label>
-                    <input className="outline-none border-2 border-zinc-200 rounded-xl w-full py-3 px-4 text-lg font-lato font-semibold text-zinc-800 leading-tight transition-all focus:border-blue-500" id="country" type="text" name="country" value="India" readOnly />
-                  </div>
-                  <div className="drop-shadow-md">
-                    <label className="block text-lg font-lato font-semibold mb-1" for="state">State</label>
-                    <input className="outline-none border-2 border-zinc-200 rounded-xl w-full py-3 px-4 text-lg font-lato font-semibold text-zinc-800 leading-tight transition-all focus:border-blue-500" id="state" type="text" name="state" value="Assam" readOnly />
-                  </div>
-                  <div className="drop-shadow-md">
-                    <label className="block text-lg font-lato font-semibold mb-1" for="city">City</label>
-                    <input className="outline-none border-2 border-zinc-200 rounded-xl w-full py-3 px-4 text-lg font-lato font-semibold text-zinc-800 leading-tight transition-all focus:border-blue-500" id="city" type="text" name="city" required />
-                  </div>
-                </div>
-                )}
-              </div>
-                {/* <h5 className="mt-3 w-full text-center text-base font-lato font-medium">already have an account? <button type="button" className="font-semibold text-blue-500 transition-all hover:text-blue-700 underline">Sign In</button></h5> */}
-            </form>
       </div>
     </>
   )
